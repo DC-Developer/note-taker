eff1919a1b2606cda5f3d559fc18524ca27e96d2
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-11 12:33:48 -0500

    changed routes, models and ejs page files

--- controller/routes.js
+++ controller/routes.js
@@ -8,48 +8,33 @@ var Note = require('../models/notes.js');
 //require models later
 
 
-//default route rendering index.ejs
+router.post("/auth", function(req, res){
+   
+
+});
+
 router.get("/", function(req, res){
     res.render("pages/login");
 });
 
 router.get("/home", function(req, res){
-    res.render("pages/home");
+   
+    User.find({})
+        .then(function(data){
+            res.render("pages/home", {
+                users: data
+            });
+        })
+        .catch(function(err){
+            res.json(err);
+        })
 });
 
 router.get("/notes", function(req, res){
     res.render("pages/notes");
 });
 //make endpoints for api
-router.post("/auth", function(req, res){
-    //take the req.body and add it to the username key field
-    
 
-    // User.findOne({ email: req.body.email }, function(err, user) {
-    //     if (err) throw err;
-    
-    //     // test a matching password
-    //     user.comparePassword('Password123', function(err, isMatch) {
-    //         if (err) throw err;
-    //         console.log('Password123:', isMatch); // -&gt; Password123: true
-    //     });
-    
-    //     // test a failing password
-    //     user.comparePassword('123Password', function(err, isMatch) {
-    //         if (err) throw err;
-    //         console.log('123Password:', isMatch); // -&gt; 123Password: false
-    //     });
-    // });
-    User.create(req.body)
-        .then(function(dbUser) {
-            console.log(dbUser);
-            //if user is created successfully, render the home page
-            res.render("pages/home");
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
 
 
 module.exports = router; 
--- models/user.js
+++ models/user.js
@@ -1,61 +1,43 @@
-var mongoose = require('mongoose'), 
-    bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
-
+var mongoose = require('mongoose');
+// , 
+//     bcrypt = require('bcrypt'),
+//     SALT_WORK_FACTOR = 10;
+//add in encryption and password later
 var schema = mongoose.Schema;
 
 var UserSchema = new schema({
+    username: {
+        type: String,
+        required: "Username required!",
+        index: {unique: true},
+        validate: [
+            function(input) {
+              return input.length >= 6;
+            },
+            "Username should be longer."
+          ]
+    },
     email: {
         type: String,
         match: [/.+\@.+\..+/, "Please enter a valid e-mail address"],
         required: true,
         index: {unique: true}
     },
-    password: {
-        type: String,
-        required: "Password is Required",
-        validate: [
-            function(input) {
-              return input.length >= 6;
-            },
-            "Password should be longer."
-          ]
-    },
+    // password: {
+    //     type: String,
+    //     required: "Password is Required",
+    //     validate: [
+    //         function(input) {
+    //           return input.length >= 6;
+    //         },
+    //         "Password should be longer."
+    //       ]
+    // },
     date: {
         type: Date,
         default: Date.now
       }
 });
-
-UserSchema.pre('save', function(next) {
-    var user = this;
-
-// only hash the password if it has been modified (or is new)
-if (!user.isModified('password')) return next();
-
-// generate a salt
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-        if (err) return next(err);
-
-        // override the cleartext password with the hashed one
-        user.password = hash;
-        next();
-    });
-});
-
-
-});
-
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
-    });
-};
 
 var User = mongoose.model("User", UserSchema);
 
