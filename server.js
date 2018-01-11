30f4b6dfb7ba7e5c21dc39e9e6f51ebc70a1f552
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-11 06:11:12 -0500

    added more to file structure and finished server file

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var app = express();
+var router = require("./controller/routes.js");
+
+var MONGOLAB_URI = "";
+var PORT = process.env.MONGOLAB_URI || 3000;
+//set mongoose to handle ES6 promises
+mongoose.Promise = Promise;
+if(process.env.MONGOLAB_URI){
+    mongoose.connect(MONGOLAB_URI);
+}else{
+    mongoose.connect("mongodb://localhost/ejsnotes");
+}
+
+var db = mongoose.connection;
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use("/", router);
+
+app.set('view engine', 'ejs');
+
+
+app.listen(PORT, function(){
+    console.log("Successfully connected to port: " + PORT);
+});
